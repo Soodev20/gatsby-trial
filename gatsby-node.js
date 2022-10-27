f5f9dadf26097d7f57c53f82e78c62ebda92d95d
soodev20 <soodev20@gmail.com>
2022-10-27 23:18:30 +0900

    Initial commit, settings

---
+++ gatsby-node.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
+  const output = getConfig().output || {};
+
+  actions.setWebpackConfig({
+    output,
+    resolve: {
+      alias: {
+        components: path.resolve(__dirname, 'src/components'),
+        utils: path.resolve(__dirname, 'src/utils'),
+        hooks: path.resolve(__dirname, 'src/hooks'),
+      },
+    },
+  });
+};
+
+exports.onCreateBabelConfig = ({ actions }) => {
+  actions.setBabelPlugin({
+    name: '@babel/plugin-transform-react-jsx',
+    options: {
+      runtime: 'automatic',
+    },
+  });
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import { FunctionComponent } from 'react'
+
+const IndexPage: FunctionComponent = function () {
+  return <h1>hi</h1>
+}
+
+export default IndexPage
